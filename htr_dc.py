94f5efb66d71ed0301f062874f51a61833b1e65d
unknown <aarvai@ARVAI-LAPTOP.occ.harvard.edu>
2014-05-30 12:45:32 -0400

    Revamped on/off ranges, Fixed monthly means, changed timeframes
    On and off ranges are now optional and include upper and lower bounds
    Monthly means were nans if they included days without cycles
    Default start time is now launch
    Only plot event if it exists (otherwise x-axis was off)
    Commented out duty cycle plot using step function
    Resurrected other duty cycle plot
    Added zoom-in on last 60 days of accumuluated htr pwr plot

--- htr_dc.py
+++ htr_dc.py
@@ -3,7 +3,7 @@ import time
 from kadi import events
 from utilities import append_to_array, find_first_after, find_last_before, find_closest
 
-def htr_dc(temp, on_range, off_range, t_start='2000:001', t_stop=None, name=None, event=None, plot_cycles=False, dur_lim=None):
+def htr_dc(temp, t_start='1999:204', t_stop=None, on_range=None, off_range=None, name=None, event=None, plot_cycles=False, dur_lim=None):
     
     close('all')
     t0 = time.time()    
@@ -24,11 +24,17 @@ def htr_dc(temp, on_range, off_range, t_start='2000:001', t_stop=None, name=None
     local_min[dt1_n0[local_min_i+1]] = 1
     local_max[dt1_n0[local_max_i]+1] = 1
     
-    htr_on_range = x.vals < on_range
-    htr_off_range = x.vals > off_range
-    
-    htr_on = local_min & htr_on_range
-    htr_off = local_max & htr_off_range
+    if on_range != None:
+        htr_on_range = (x.vals > on_range[0]) & (x.vals < on_range[1])
+        htr_on = local_min & htr_on_range
+    else:    
+        htr_on = local_min 
+
+    if off_range != None:
+        htr_off_range = (x.vals > off_range[0]) & (x.vals < off_range[1])      
+        htr_off = local_max & htr_off_range
+    else:
+        htr_off = local_max
     
     #remove any incomplete heater cycles at end of timeframe
     t2 = time.time()
@@ -69,8 +75,9 @@ def htr_dc(temp, on_range, off_range, t_start='2000:001', t_stop=None, name=None
     t4 = time.time()
     on_dates = DateTime(t_on).iso
     on_dates_days = floor(DateTime(t_on).mjd)
+    on_dates_mos = array([date[0:7] for date in on_dates])
     
-    days = arange(floor(DateTime(t_on[0]).mjd), DateTime(t_on[-1]).mjd)
+    days = arange(floor(DateTime(t_start).mjd), DateTime(t_stop).mjd)
     days_dates = DateTime(days, format='mjd').iso
     days_mos = array([date[0:7] for date in days_dates])
     mos = unique(days_mos)
@@ -81,25 +88,28 @@ def htr_dc(temp, on_range, off_range, t_start='2000:001', t_stop=None, name=None
     #compute stats
     t5 = time.time()
     on_freq = array([sum(on_dates_days == day) for day in days])
+    on_freq[isnan(on_freq)] = 0
     on_freq_mo_mean = array([mean(on_freq[days_mos == mo]) for mo in mos])
     
     on_time = array([sum(dur_each[on_dates_days == day]) for day in days])
+    on_time[isnan(on_time)] = 0
     on_time_mo_mean = array([mean(on_time[days_mos == mo]) for mo in mos])
     
     dur = array([mean(dur_each[on_dates_days == day]) for day in days])
-    dur_mo_mean = array([mean(dur[days_mos == mo]) for mo in mos])
+    dur_mo_mean = array([mean(dur_each[on_dates_mos == mo]) for mo in mos])
     
     acc_pwr = array([sum(pwr[on_dates_days == day]) for day in days])
+    acc_pwr[isnan(acc_pwr)] = 0
     acc_pwr_mo_mean = array([mean(acc_pwr[days_mos == mo]) for mo in mos])
     
     per_each = diff(t_on)
     per = array([mean(per_each[on_dates_days[:-1] == day]) for day in days])
-    per_mo_mean = array([mean(per[days_mos == mo]) for mo in mos])
+    per_mo_mean = array([mean(per_each[on_dates_mos[:-1] == mo]) for mo in mos])
     
     dc_each = dur_each[:-1] / per_each * 100
     dc = on_time / (3600*24) * 100
     dc_mo_mean = array([mean(dc[days_mos == mo]) for mo in mos])
-    
+ 
     #plots
     t6 = time.time()
     
@@ -111,7 +121,8 @@ def htr_dc(temp, on_range, off_range, t_start='2000:001', t_stop=None, name=None
         plot_cxctime(x.times, x.vals, 'b*',mew=0)
         plot_cxctime(x.times[htr_on], x.vals[htr_on], 'c*',mew=0, label='Heater On')
         plot_cxctime(x.times[htr_off], x.vals[htr_off], 'r*',mew=0, label='Heater Off')
-        plot_cxctime(t_event, ylim(),'r:')
+        if event != None:
+            plot_cxctime(t_event, ylim(),'r:')
         ylabel('deg F')
         #legend(loc=0)
         title(name + ' Heater Cycling per ' + temp)
@@ -130,7 +141,8 @@ def htr_dc(temp, on_range, off_range, t_start='2000:001', t_stop=None, name=None
     plot_cxctime(t_days[:-1], dur[:-1]/60, 'b', alpha=.5, label='Daily Mean')
     #omit last month in this case due to small sample size
     plot_cxctime(t_mos[:-1], dur_mo_mean[:-1]/60, 'k', label='Monthly Mean')
-    plot_cxctime(t_event, ylim(),'r:')
+    if event != None:    
+        plot_cxctime(t_event, ylim(),'r:')
     title(name + ' Heater On-Time')
     ylabel('min')
     legend(loc=0)
@@ -141,35 +153,41 @@ def htr_dc(temp, on_range, off_range, t_start='2000:001', t_stop=None, name=None
     plot_cxctime(t_days[:-1], per[:-1]/60, 'b', alpha=.5, label='Daily Mean')
     #omit last month in this case due to small sample size
     plot_cxctime(t_mos[:-1], per_mo_mean[:-1]/60, 'k', label='Monthly Mean')
-    plot_cxctime(t_event, ylim(),'r:')
+    if event != None:    
+        plot_cxctime(t_event, ylim(),'r:')
     title(name + ' Heater Period')
     ylabel('min')
     legend(loc=0)
     savefig('htr_' + temp + '_period.png')
     
-    figure(5, figsize=(8,4))
-    dc_each_step = append_to_array(dc_each, pos=0, val=dc_each[0])
-    step(t_on, dc_each_step, 'r')
-    title(name + ' Heater Duty Cycle')
-    ylabel('%')
-    savefig('htr_' + temp + '_duty_cycle_step.png')
+    #figure(5, figsize=(8,4))
+    #as a first-order hold, this plotting is more accurate than fig 6
+    #(holds values til the next), but isn't necessary if there's a long
+    #timespan.  Disadvantage is the unhelpful x-axis.
+    #dc_each_step = append_to_array(dc_each, pos=0, val=dc_each[0])
+    #step(t_on, dc_each_step, 'r')
+    #title(name + ' Heater Duty Cycle')
+    #ylabel('%')
+    #savefig('htr_' + temp + '_duty_cycle_step.png')
 
     figure(6, figsize=(8,4))
-    plot_cxctime(t_on[:-1], dc_each, 'r', label='Range')
-    #plot_cxctime(t_days[:-1], dc[:-1], 'b', alpha=.5, label='Daily Mean')
+    plot_cxctime(t_on[:-1], dc_each, 'r', alpha=.2, label='Range')
+    plot_cxctime(t_days[:-1], dc[:-1], 'b', alpha=.5, label='Daily Mean')
     #omit last month in this case due to small sample size
-    #plot_cxctime(t_mos[:-1], dc_mo_mean[:-1], 'k', label='Monthly Mean') 
-    plot_cxctime(t_event, ylim(),'r:')
+    plot_cxctime(t_mos[:-1], dc_mo_mean[:-1], 'k', label='Monthly Mean') 
+    if event != None:    
+        plot_cxctime(t_event, ylim(),'r:')
     title(name + ' Heater Duty Cycle')
     ylabel('%')
-    #legend(loc=0)
+    legend(loc=0)
     savefig('htr_' + temp + '_duty_cycle.png')
     
     figure(7)
     plot_cxctime(t_days[:-1], on_freq[:-1], 'b', alpha=.3, label='Range')
     #omit last month in this case due to small sample size
     plot_cxctime(t_mos[:-1], on_freq_mo_mean[:-1], 'k', label='Monthly Mean')
-    plot_cxctime(t_event, ylim(),'r:')
+    if event != None:    
+        plot_cxctime(t_event, ylim(),'r:')
     title(name + ' Heater Cycles Per Day')
     legend(loc=0)
     savefig('htr_' + temp + '_on_freq.png')
@@ -178,21 +196,25 @@ def htr_dc(temp, on_range, off_range, t_start='2000:001', t_stop=None, name=None
     plot_cxctime(t_days[:-1], on_time[:-1]/3600, 'b', alpha=.3, label='Range')
     #omit last month in this case due to small sample size
     plot_cxctime(t_mos[:-1], on_time_mo_mean[:-1]/3600, 'k', label='Monthly Mean')
-    plot_cxctime(t_event, ylim(),'r:')
+    if event != None:    
+        plot_cxctime(t_event, ylim(),'r:')
     title('Accumulated ' + name + ' Heater On-Time Per Day')
     ylabel('hrs')
     legend(loc=0)
     savefig('htr_' + temp + '_acc_on_time.png')
     
-    figure(9)
+    figure(9, figsize=(8,3))
     plot_cxctime(t_days[:-1], acc_pwr[:-1], 'b', alpha=.3, label='Range')
     #omit last month in this case due to small sample size
     plot_cxctime(t_mos[:-1], acc_pwr_mo_mean[:-1], 'k', label='Monthly Mean')
-    plot_cxctime(t_event, ylim(),'r:')
+    if event != None:    
+        plot_cxctime(t_event, ylim(),'r:')
     title('Accumulated ' + name + ' Heater Power Per Day')
     ylabel('W-hrs')
     legend(loc=0)
     savefig('htr_' + temp + '_acc_pwr.png')
+    xlim([DateTime(xlim()[1]-60, format='plotdate').plotdate, xlim()[1]])
+    savefig('htr_' + temp + '_acc_pwr_zoom.png')
     
     print('Processing times:')
     print(str(t1 - t0) + ' - fetching data')
